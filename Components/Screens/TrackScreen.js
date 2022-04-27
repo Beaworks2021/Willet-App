129fee03bc754f94543efe9ed6216a6e3a7d2a1e
Beaworks2021 <ekowworks@gmail.com>
2022-04-27 17:20:53 +0000

    fix error

--- Components/Screens/TrackScreen.js
+++ Components/Screens/TrackScreen.js
@@ -8,7 +8,7 @@ const TrackScreen = () => {
   console.log(userData);
   return (
     <View>
-      {userData?.packages.length === 0 ? (
+      {!userData?.packages ? (
         <Text>No Package</Text>
       ) : (
         <FlatList
