70692bcfcb323ad3dd1302bc8e05f15337d745f5
Cobboldgit <augustinecobbold6@gmail.com>
2022-04-27 06:47:43 +0000

    added getUserData action and sync package data

--- Components/Redux/actions/userActions.js
+++ Components/Redux/actions/userActions.js
@@ -19,3 +19,51 @@ export const setTravelTimeInformation = (state) => {
     payload: state,
   };
 };
+
+export const syncPackageData = ({}) => {
+  return (dispatch, useState, { getFirestore, getFirebase }) => {
+    const userId = getFirebase().auth().currentUser.uid;
+    const db = getFirestore();
+    const dbRef = db.collection("users").doc(userId);
+
+    let data = {};
+
+    dbRef
+      .update({
+        packages: firebase.firestore.FieldValue.arrayUnion(data),
+      })
+      .then(() => {
+        alert("Data synced");
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+};
+
+export const getUserData = () => {
+  return (dispatch, useState, { getFirestore, getFirebase }) => {
+    const userId = getFirebase().auth().currentUser.uid;
+    const db = getFirestore();
+    const dbRef = db.collection("users").doc(userId);
+
+    dbRef.onSnapshot(
+      (querySnapshot) => {
+        // array to store returned data
+        let userData = [];
+
+        // push returned data from database to trans array
+        userData.push(querySnapshot.data());
+
+        // make data available for redux state
+        dispatch({
+          type: "GET_USER_DATA",
+          payload: userData,
+        });
+      },
+      (error) => {
+        alert(error.message);
+      }
+    );
+  };
+};
