c703e2716daa31fe2d66a370e1e557e8fbcb717f
Cobboldgit <augustinecobbold6@gmail.com>
2022-04-27 06:49:35 +0000

    fetch userData in TrackScreen

--- Components/Screens/TrackScreen.js
+++ Components/Screens/TrackScreen.js
@@ -1,7 +1,11 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { useSelector } from "react-redux";
 
 const TrackScreen = () => {
+  const {userData} = useSelector((state) => state.userReducers)
+
+  console.log(userData);
   return (
     <View>
       <Text>TrackScreen</Text>
